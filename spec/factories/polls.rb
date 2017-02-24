e7ae292fe1831d29e5579473cb6adb5e9e92f0b9
Sunny <sunnyinvadeher@gmail.com>
2017-02-24 15:48:39 +0800

    added factory definition of question and user

--- spec/factories/polls.rb
+++ spec/factories/polls.rb
@@ -1,17 +1,7 @@
-FactoryGirl.define do
+FactoryGirl.define do 
 
-  factory :user do
-    sequence(:email) { |n| "user#{n}@test.com" }
-    password "123456"
-    password_confirmation "123456"
-  end
-
-  factory :admin, class: User do
-    sequence(:email) { |n| "admin#{n}@test.com" }
-    password "123456"
-    password_confirmation "123456"
-    admin true
+  factory :poll do
+    title "Test_Poll"
   end
 
 end
-    
---
+++ spec/factories/questions.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do 
+
+  factory :question do
+    statement "blahblahblah....."
+    poll # belongs to poll
+  end
+end
