5e4c3bbe2f1bad4b9841c921e91936ecd95ae9f4
Sunny <sunnyinvadeher@gmail.com>
2017-02-24 15:51:42 +0800

    simply modified syntax style

--- spec/model/answer_spec.rb
+++ spec/model/answer_spec.rb
@@ -3,7 +3,7 @@ require 'answer'
 
 describe Answer, type: :model do
 
-  it {is_expected.to belong_to(:user)}
-  it {is_expected.to belong_to(:question)}
+  it { is_expected.to belong_to(:user) }
+  it { is_expected.to belong_to(:question) }
 
 end
