359887eb7984488697ae958d0257d05216863d8f
Sunny <sunnyinvadeher@gmail.com>
2017-02-24 15:09:23 +0800

    modified controller_macros.rb to use FactoryGirl

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -3,12 +3,7 @@ module ControllerMacros
   def login_admin
     before(:each) do
       @request.env["devise.mapping"] = Devise.mappings[:user]
-      user = User.new
-      user.email = "rspec_admin@test.com"
-      user.password = "123456"
-      user.password_confirmation = "123456"
-      user.admin = true
-      user.save!
+      user = FactoryGirl.create(:admin)
       sign_in user
     end
   end
@@ -16,15 +11,8 @@ module ControllerMacros
   def login_user
     before(:each) do
       @request.env["devise.mapping"] = Devise.mappings[:user]
-      user = User.new
-      user.email = "rspec@test.com"
-      user.password = "123456"
-      user.password_confirmation = "123456"
-      user.save!
+      user = FactoryGirl.create(:user)
       sign_in user
-      # user = FactoryGirl.create(:user)
-      # user.confirm! # or set a confirmed_at inside the factory. Only necessary if you are using the "confirmable" module
-      # sign_in user
     end
   end
 end
